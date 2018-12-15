0046e48a68525bad87128e1e2610a7cf0d1bc5d4
kabirules <javifont@gmail.com>
2018-12-15 23:44:59 +0100

    Added HTTP PRO + requests

---
+++ Assets/Scripts/FortniteStatsManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BestHTTP;
+using System;
+using m = Model;
+using LitJson;
+
+public class FortniteStatsManager : MonoBehaviour {
+
+	private string userId;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// GETUSERID
+	public void GetUserId() {
+		string url = "https://fortnite-public-api.theapinetwork.com/prod09/users/id?username=Ninja";
+		HTTPRequest request = new HTTPRequest (new Uri (url), OnRequestGetUserIdFinished);
+		request.Send ();
+	}
+
+	void OnRequestGetUserIdFinished(HTTPRequest request, HTTPResponse response)
+	{
+		Debug.Log(response.DataAsText);
+		m.UserId userIdResponse = JsonMapper.ToObject<m.UserId>(response.DataAsText);
+		this.userId = userIdResponse.uid;
+	}
+
+	// GETSTATS
+	public void GetStats() {
+		string url = "https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats?user_id=4735ce9132924caf8a5b17789b40f79c&platform=pc&window=alltime";
+		HTTPRequest request = new HTTPRequest (new Uri (url), OnRequestGetStatsFinished);
+		// request.SetHeader("Authorization","ea1e9f837f0a6b6679e4b90985cf5302");
+		request.Send ();
+	}
+
+	void OnRequestGetStatsFinished(HTTPRequest request, HTTPResponse response)
+	{
+		m.UserStats userStatsResponse = JsonMapper.ToObject<m.UserStats>(response.DataAsText);
+		Debug.Log(userStatsResponse.uid);
+		Debug.Log(userStatsResponse.username);
+		Debug.Log(userStatsResponse.stats.kills_solo);
+		Debug.Log(userStatsResponse.totals.kills);
+	}	
+}
---
+++ Assets/Scripts/Model.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Model : MonoBehaviour {
+
+	public class UserId {
+		public string uid { get; set; }
+		public string username { get; set; }
+		public string[] platforms { get; set; }
+		public string[] seasons { get; set; }
+		/*
+		{
+			"uid": "4735ce9132924caf8a5b17789b40f79c",
+			"username": "Ninja",
+			"platforms": [
+				"pc"
+			],
+			"seasons": [
+				"season7",
+				"season6",
+				"season5",
+				"season4"
+			]
+		}
+		*/
+	}
+
+	public class Stats {
+		public Int32 kills_solo { get; set; }
+		public Int32 matchesplayed_solo { get; set; }
+		public Double winrate_solo { get; set; }
+		public Int32 score_solo { get; set; }
+	}
+
+	public class Totals {
+		public Int32 kills { get; set; }
+	}	
+
+	public class UserStats {
+		public string uid { get; set; }
+		public string username { get; set; }
+		public Stats stats { get; set; }
+		public Totals totals { get; set; }
+		/*
+		{
+			"cached": true,
+			"uid": "4735ce9132924caf8a5b17789b40f79c",
+			"username": "Ninja",
+			"platform": "pc",
+			"timestamp": 1544911933,
+			"window": "alltime",
+			"stats": {
+				"kills_solo": 38494,
+				"placetop1_solo": 1814,
+				"placetop10_solo": 2458,
+				"placetop25_solo": 2961,
+				"matchesplayed_solo": 5225,
+				"kd_solo": 11.29,
+				"winrate_solo": 34.72,
+				"score_solo": 1736401,
+				"minutesplayed_solo": 24386,
+				"lastmodified_solo": 1544899923,
+				"kills_duo": 28747,
+				"placetop1_duo": 1696,
+				"placetop5_duo": 2082,
+				"placetop12_duo": 2466,
+				"matchesplayed_duo": 4128,
+				"kd_duo": 11.82,
+				"winrate_duo": 41.09,
+				"score_duo": 1749293,
+				"minutesplayed_duo": 17407,
+				"lastmodified_duo": 1544900281,
+				"kills_squad": 26019,
+				"placetop1_squad": 1388,
+				"placetop3_squad": 1667,
+				"placetop6_squad": 1948,
+				"matchesplayed_squad": 3948,
+				"kd_squad": 10.16,
+				"winrate_squad": 35.16,
+				"score_squad": 1681113,
+				"minutesplayed_squad": 8298,
+				"lastmodified_squad": 1544910301
+			},
+			"totals": {
+				"kills": 93260,
+				"wins": 4898,
+				"matchesplayed": 13301,
+				"minutesplayed": 50091,
+				"hoursplayed": 835,
+				"score": 5166807,
+				"winrate": 36.82,
+				"kd": 11.1,
+				"lastupdate": 1544899923
+			}
+		}		
+		 */
+	}
+
+}
